85d4f695de5192ea901d528b1e204eb0ac37f278
Geert Bevin <geert@eigenlabs.com>
2012-11-09 09:59:41 +0100

    Added support for cancelling all talker phrases from a particular talker on a recorder.

--- plg_simple/talker_plg.py
+++ plg_simple/talker_plg.py
@@ -256,7 +256,7 @@ class Key(collection.Collection):
 
 class Agent(agent.Agent):
     def __init__(self, address, ordinal):
-        agent.Agent.__init__(self,signature=version,names='talker',ordinal=ordinal)
+        agent.Agent.__init__(self,signature=version,names='talker',ordinal=ordinal,protocols='talker')
 
         self.add_verb2(2,'do([],None,role(None,[abstract]),role(when,[singular,numeric]),option(called,[singular,numeric]))', self.__do_verb)
         self.add_verb2(8,'cancel([],None,role(None,[singular,numeric]),option(called,[singular,numeric]))', self.__cancel_verb)
